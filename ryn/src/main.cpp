7a1fdcb2a4ff2a3b0f8a838b0b4c42d3c2ce0c02
ReinoutWW <reinoutwijnholds2002@gmail.com>
2025-02-02 19:16:19 +0100

    Add file read

--- ryn/src/main.cpp
+++ ryn/src/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
-int main() {
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cout << "Incorrect usage. The correct usage:" << std::endl;
+        std::cout << "ryn <input.ryn>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::string contents;
+    {
+        std::stringstream contents_stream;
+        std::fstream input(argv[1], std::ios::in);
+        contents_stream << input.rdbuf();
+        contents = contents_stream.str();
+    }
+
+    std::cout << contents << std::endl;
+
+    return EXIT_SUCCESS;
 }
