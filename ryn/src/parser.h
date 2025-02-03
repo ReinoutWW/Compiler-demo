3f4af8bbfe1f544c610182c0d28c60b1fa2e5de9
ReinoutWW <reinoutwijnholds2002@gmail.com>
2025-02-03 22:02:12 +0100

    Create expression parser

--- ryn/src/parser.h
+++ ryn/src/parser.h
@@ -1,11 +1,50 @@
 #pragma once
 
+#include "tokenization.h"
+
+struct NodeExpr {
+    Token int_lit;
+};
+
+struct NodeExit {
+    NodeExpr expr;
+};
+
 class Parser {
 public:
     inline explicit Parser(std::vector<Token> tokens)
         : m_tokens(std::move(tokens))
     {
 
+    }
+
+    std::optional<NodeExpr> parse_expr() {
+        if (peak().has_value() && peak().value().type == TokenType::_int_lit) {
+            return std::make_optional(NodeExpr{ consume() });
+        }
+
+        return std::nullopt;
+    }
+
+    std::optional<NodeExit> parse() {
+        while (peak().has_value()) {
+            if (peak().value().type == TokenType::_exit) {
+                consume();
+                if (auto node_expr = parse_expr()) {
+                    std::optional<NodeExit> exit_node = NodeExit{.expr = node_expr.value()};
+                } else {
+                    std::cerr << "Invalid Expression" << std::endl;
+                    exit(EXIT_FAILURE);
+                }
+                if (!peak().has_value() || peak().value().type != TokenType::_semi) {
+                    std::cerr << "Invalid Expression" << std::endl;
+                    exit(EXIT_FAILURE);
+                }
+            }
+        }
+
+        m_index = 0;
+        return NodeExit();
     }
 
 private:
